d73faa343c93e3c641f4456df4540f81ff538acf
Roman <romanmarkilov@gmail.com>
2024-04-09 01:59:58 +0300

    added basic form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import InputField from "./components/InputField";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <h1 className="font-serif font-normal">Contact us</h1>
+      <InputField />
     </div>
   );
 };
