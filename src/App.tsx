8e11c7abb0c6618d19df0db7f94387c3e10af5cd
Roman <romanmarkilov@gmail.com>
2024-04-10 19:53:43 +0300

    added tailwind, structure changed, added css, corrected inputfield, added left div

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import React from "react";
-import "./App.css";
-import InputField from "./components/InputField";
+import "./styles.css";
+import InputField from "./InputField";
 
-const App: React.FC = () => {
+export default function App() {
   return (
     <div className="App">
-      <h1 className="font-serif font-normal">Contact us</h1>
-      <InputField />
+      <div className="grid grid-cols-1 mt-[6%] tall:mt-[3%] tall-md:mt-[6%] tall:pb-[3%] pb-[6%] lg:[grid-template-columns:35%_65%] sm:px-10 md:px-16 lg:px-10 xl:px-20">
+        <aside>
+          <div className="space-y-4 pb-7 text-center sm:text-left md:space-y-7 lg:pb-0">
+            <div>
+              <p className="text-xl md:font-reckless-neue md:text-4xl">
+                Media enquiries:
+              </p>
+              <a href="/">press@tuum.com</a>
+            </div>
+            <div>
+              <p className="text-xl md:font-reckless-neue md:text-4xl">
+                Career questions:
+              </p>
+              <a href="/">careers@tuum.com</a>
+            </div>
+          </div>
+        </aside>
+        <InputField />
+      </div>
     </div>
   );
-};
-
-export default App;
+}
---
+++ src/InputField.tsx
@@ -0,0 +1,64 @@
+import "./styles.css";
+
+const inputField = () => {
+  return (
+    <div className="bg-white tall:py-6 tall-md:py-8 py-8 px-8 lg:px-10 xl:px-16 rounded-[40px]">
+      <h1 className="mb-4">Contact us</h1>
+      <div className="max-h-screen flex flex-wrap -mx-2 mb-4">
+        <div className="w-full md:w-1/2 px-2 mb-4 md:mb-0">
+          <input
+            type="text"
+            placeholder="First Name"
+            className="input_box"
+          ></input>
+        </div>
+        <div className="w-full md:w-1/2 px-2">
+          <input
+            type="text"
+            placeholder="Last Name"
+            className="input_box"
+          ></input>
+        </div>
+      </div>
+      <div className="max-h-screen flex flex-wrap -mx-2 mb-4">
+        <div className="w-full md:w-1/2 px-2 mb-4 md:mb-0">
+          <input type="text" placeholder="Email" className="input_box"></input>
+        </div>
+        <div className="w-full md:w-1/2 px-2">
+          <input
+            type="text"
+            placeholder="Job Title"
+            className="input_box"
+          ></input>
+        </div>
+      </div>
+      <div className="max-h-screen flex flex-wrap -mx-2 mb-4">
+        <div className="w-full md:w-1/2 px-2 mb-4 md:mb-0">
+          <input
+            type="text"
+            placeholder="Country"
+            className="input_box"
+          ></input>
+        </div>
+        <div className="w-full md:w-1/2 px-2">
+          <input
+            type="text"
+            placeholder="Company name"
+            className="input_box"
+          ></input>
+        </div>
+      </div>
+      <div className="hs-input hs-fieldtype-textarea">
+        <textarea
+          placeholder="What would you like to talk about?"
+          className="input_box"
+        ></textarea>
+      </div>
+      <button type="submit" className="input_submit">
+        Submit
+      </button>
+    </div>
+  );
+};
+
+export default inputField;
